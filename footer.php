c69cf17bc595e99018ae2b3e661092e86df3d6fd
Lukky <lukky.business@gmail.com>
2021-05-08 17:11:45 +0200

    little clean up of the footer

--- footer.php
+++ footer.php
@@ -4,13 +4,11 @@
     <div class="left">
         <h3>Contact </h3>
 <?php
-    // query for the about page
     $your_query = new WP_Query( 'pagename=Contact' );
-    // "loop" through query (even though it's just one page) 
     while ( $your_query->have_posts() ) : $your_query->the_post();
         the_content();
     endwhile;
-    // reset post data (important!)
+
     wp_reset_postdata();
 ?>
 </div>
