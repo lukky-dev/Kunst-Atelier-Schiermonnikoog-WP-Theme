a160ff8a0c8db3f5629cf6cf53d8212fcb7cec26
Lukky <lukky.business@gmail.com>
2021-05-09 16:11:47 +0200

    made workshop overview and single workshop better looking

--- archive.php
+++ archive.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="wrapper" style="padding-top:180px">
-    <h1> Workshops </h1><hr>
+    <h1> Workshops </h1>
     <div class="workshop-list">
         <?php get_template_part('includes/section','workshop'); ?>
     </div>
